c27fb0df2762b0fd296515afec4298f60d1cb226
MengchunLin <smailjumltsr0@gmail.com>
2024-10-14 00:24:54 +0800

    調整分兩層的邏輯

--- 111.py
+++ 111.py
@@ -2,55 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from pathlib import Path
+import numpy as np
 
-
-# 获取当前脚本目录
+# 设置文件路径
 script_dir = Path('.')
-
-# 搜索 Excel 文件
-file_1_name = '02-soil_depth_statistics_with_ic_and_qc_avg_skipping_200.xlsx'
-file_2_name = '04-soil_depth_statistics_with_ic_and_slope_skipping_200.xlsx'
-
-# 组合目录与文件名
-file_1_path = script_dir / file_1_name
-file_2_path = script_dir / file_2_name
+file_1_path = script_dir / '02_for_predict_data.xlsx'
+file_2_path = script_dir / '04_for_predict_data.xlsx'
 
 borehole_position_1 = 1
 borehole_position_2 = 1558.53
 
-# 检查文件是否存在
-if not file_1_path.exists() or not file_2_path.exists():
-    raise FileNotFoundError("One or both of the files were not found in the script directory.")
-
-# 读取两个文件
+# 读取文件
 df_1 = pd.read_excel(file_1_path)
 df_2 = pd.read_excel(file_2_path)
-
-
-# 定义深度范围分段
-sections_df_1 = [
-    {"name": "Section 11", "min_depth": 0, "max_depth": 60},
-]
-
-sections_df_2 = [
-    {"name": "Section 21", "min_depth": 0, "max_depth": 60},
-]
-
-def segment_data(df, sections):
-    segmented_data = {}
-    for section in sections:
-        section_df = df[(df['Upper Depth'] >= section['min_depth']) & (df['Upper Depth'] < section['max_depth'])]
-        if section['min_depth'] == 0 and not section_df.empty:
-            section_df.loc[section_df.index[0], 'Upper Depth'] = 0
-        segmented_data[section['name']] = section_df.reset_index(drop=True)
-    return segmented_data
-
-# 分段
-segmented_df_1 = segment_data(df_1, sections_df_1)
-segmented_df_2 = segment_data(df_2, sections_df_2)
-
-# 比较分段数据框中的相同土壤类型
-layers = []  # 用于存储层信息
 
 # 定义颜色映射
 color_mapping = {
@@ -61,95 +25,220 @@ color_mapping = {
     '5': 'tan',
 }
 
-# 遍历每个分段
-for section1, section2 in zip(segmented_df_1.values(), segmented_df_2.values()):
-    length = min(len(section1), len(section2))  # 确保不会超出短表的长度
+# 初始化变量
+layers = []
+legend_labels = set()
+matched_layers_1 = set()
+matched_layers_2 = set()
+depth_ranges = [(0, 60)]  # 定义深度区间
 
-    for i in range(length):
-        # 确保访问的索引在范围内
-        if i >= len(section1) or i >= len(section2):
-            break  # 退出循环以避免索引超出范围
+upper_depth_draw_1 = 0
+upper_depth_draw_2 = 0
+lower_depth_draw_1 = 0
+lower_depth_draw_2 = 0
 
-        # 获取土壤类型和深度信息
-        type1 = section1['Type'].iloc[i]
-        type2 = section2['Type'].iloc[i]
-        upper1 = section1['Upper Depth'].iloc[i]
-        upper2 = section2['Upper Depth'].iloc[i]
-        lower1 = section1['Lower Depth'].iloc[i]
-        lower2 = section2['Lower Depth'].iloc[i]
-        count1 = section1['Type'].value_counts()
-        count2 = section2['Type'].value_counts()
-        # 确定颜色
-        color = color_mapping.get(str(type1))
+for depth_start, depth_end in depth_ranges:
+    section_df_1 = df_1[(df_1['Upper Depth'] > depth_start) & (df_1['Lower Depth'] <= depth_end)]
+    section_df_2 = df_2[(df_2['Upper Depth'] > depth_start) & (df_2['Lower Depth'] <= depth_end)]
 
-        # 绘制第一个层
-        if i == 0:
-            layers.append({
-                "name": type1,
-                "color": color,
-                "points": [(borehole_position_1, 0), (borehole_position_2, 0), (borehole_position_2, lower2), (borehole_position_1, lower1)]
-            })
-            count1[type1] -= 1
-            count2[type2] -= 1
-        #如果土讓數量相同
-        if count1[type1] == count2[type2]:
-            #直接找到第一個相同的土壤做連線
-            for j in range(i, length):
-                if section1['Type'].iloc[i] == section2['Type'].iloc[j]:
-                    upper1 = section1['Upper Depth'].iloc[j]
-                    upper2 = section2['Upper Depth'].iloc[j]
-                    lower1 = section1['Lower Depth'].iloc[j]
-                    lower2 = section2['Lower Depth'].iloc[j]
-                    layers.append({
-                        "name": type1,
-                        "color": color,
-                        "points": [(borehole_position_1, upper1), (borehole_position_2, upper2), (borehole_position_2, lower2), (borehole_position_1, lower1)]
-                    })
-                    count1[type1] -= 1
-                    count2[type2] -= 1
-                    break
-            
+    soil_type_1 = section_df_1['Type']
+    soil_type_2 = section_df_2['Type']
 
-        # 如果土壤數量不同
-        else:
-            print("type1",type1)
-            # 如果差值為1
-            if count1[type1] == 1 and count2[type2] ==0 :
+    lower_depth_1 = section_df_1['Lower Depth']
+    lower_depth_2 = section_df_2['Lower Depth']
+
+    count_1 = soil_type_1.value_counts()
+    count_2 = soil_type_2.value_counts()
+
+    for idx_1, type_1 in enumerate(soil_type_1):
+        if idx_1 in matched_layers_1:
+            continue
+        lower_depth_draw_1 = lower_depth_1.iloc[idx_1]
+
+        for idx_2 in range(len(soil_type_2)):
+            if idx_2 in matched_layers_2:
+                continue
+
+            type_2 = soil_type_2.iloc[idx_2]
+
+            # 如果順序相同，直接匹配
+            if type_1 == type_2 and idx_1 == idx_2:
+                lower_depth_draw_2 = lower_depth_2.iloc[idx_2]
                 layers.append({
-                    "name": type1,
-                    "color": color,
-                    "points": [(borehole_position_1, upper1), (borehole_position_2, previous_lower2), (borehole_position_2, previous_lower2), (borehole_position_1, lower1)]
+                    "points": [(borehole_position_1, upper_depth_draw_1),
+                               (borehole_position_2, upper_depth_draw_2),
+                               (borehole_position_2, lower_depth_draw_2),
+                               (borehole_position_1, lower_depth_draw_1)],
+                    "color": color_mapping[str(type_1)],
+                    "label": type_1 if type_1 not in legend_labels else None
                 })
-                print("type1",type1)
+                matched_layers_1.add(idx_1)
+                matched_layers_2.add(idx_2)
+                legend_labels.add(type_1)
+                upper_depth_draw_1 = lower_depth_draw_1
+                upper_depth_draw_2 = lower_depth_draw_2
+                count_1[type_1] -= 1
+                count_2[type_2] -= 1
                 break
-    # 記錄前一個土壤下限
-    previous_lower1 = lower1
-    previous_lower2 = lower2
+
+            # 數量相同，直接匹配
+            if count_1[type_1] == count_2[type_2]:
+                lower_depth_draw_1 = lower_depth_1.iloc[idx_1]
+                lower_depth_draw_2 = lower_depth_2.iloc[idx_2]
+                layers.append({
+                    "points": [(borehole_position_1, upper_depth_draw_1),
+                               (borehole_position_2, upper_depth_draw_2),
+                               (borehole_position_2, lower_depth_draw_2),
+                               (borehole_position_1, lower_depth_draw_1)],
+                    "color": color_mapping[str(type_1)],
+                    "label": type_1 if type_1 not in legend_labels else None
+                })
+                matched_layers_1.add(idx_1)
+                matched_layers_2.add(idx_2)
+                legend_labels.add(type_1)
+                upper_depth_draw_1 = lower_depth_draw_1
+                upper_depth_draw_2 = lower_depth_draw_2
+                count_1[type_1] -= 1
+                count_2[type_2] -= 1
+                break
+
+            # 數量不同，找最接近的Ic值(向下找3組)
+            else:
+                if count_1[type_1] != 0 and count_2[type_2] != 0:
+                    # 往下3行找最接近的Ic值
+                    x=0
+                    difference = []
+                    for i in range(3):
+                        #如果往下3層有1個以上的相同土層，則分為兩層
+                        if type_1 == soil_type_2.iloc[idx_2 + i]:
+                            x+=1
+                        else:
+                            difference.append(idx_2 + i)
+                    if x>1 :
+                        #檢核difference的upper depth及lower depth是否在兩層土層的範圍內
+                        for i in range(len(difference)):
+                            if section_df_2.loc[difference[i], 'Upper Depth'] >= section_df_1.loc[idx_1-1, 'Upper Depth'] and section_df_2.loc[difference[i], 'Lower Depth'] <= section_df_1.loc[idx_1-1, 'Lower Depth']:
+                                break
+                        # 把此層土分為兩層
+                        Ic_2 = section_df_2['Average Ic']
+                        half_depth=(section_df_1.loc[idx_1-1,'Upper Depth']+section_df_1.loc[idx_1-1, 'Lower Depth']) / 2
+                        bottom_depth=section_df_1.loc[idx_1-1, 'Lower Depth']
+                        #新增一層空白土層在idx_1+1
+                        # 創建一個空白行，所有欄位為 NaN
+                        blank_row = pd.DataFrame([[np.nan] * section_df_1.shape[1]], columns=section_df_1.columns)
+                        # 使用 pd.concat 在指定位置插入空白行
+                        section_df_1 = pd.concat([section_df_1.iloc[:idx_1], blank_row, section_df_1.iloc[idx_1:]]).reset_index(drop=True)
+                        section_df_1.loc[idx_1 , 'Type' ] = section_df_1.loc[idx_1-1,'Type']  # 複製上一行的值 v
+                        section_df_1.loc[idx_1 , 'Lower Depth'] = bottom_depth  # 同樣深度
+                        section_df_1.loc[idx_1 , 'Average Ic'] = section_df_1.loc[idx_1-1, 'Average Ic']  # 複製 Ic 值
+                        section_df_1.loc[idx_1 , 'Upper Depth'] = half_depth  # 新增的土層的上界
+                        section_df_1.loc[idx_1-1, 'Lower Depth'] = half_depth  # 更新原土層的下界
+                        # 上層土層連接第一個找到的相同土層
+                        layers.append({
+                            "points": [(borehole_position_1, section_df_1.loc[idx_1-1, 'Upper Depth']),
+                                        (borehole_position_2, section_df_1.loc[idx_1 , 'Upper Depth']),
+                                        (borehole_position_2, section_df_1.loc[idx_1 , 'Lower Depth']),
+                                        (borehole_position_1, section_df_1.loc[idx_1-1, 'Lower Depth'])],
+                            "color": color_mapping[str(type_1)],
+                            "label": type_1 if type_1 not in legend_labels else None
+                        })
+                        
+                        matched_layers_1.add(idx_1-1)
+                        legend_labels.add(type_1)
+                        upper_depth_draw_1 = half_depth
+
+                                
 
 
-# 绘图
+                    else:
+                        Ic_2 = section_df_2['Average Ic']
+                        target_Ic = section_df_1['Average Ic'].iloc[idx_1]
+                        closest_value = Ic_2[np.abs(Ic_2 - target_Ic).argmin()]
+                        idx_2 = Ic_2[Ic_2 == closest_value].index[0]
+                        lower_depth_draw_2 = lower_depth_2.iloc[idx_2]
+                        layers.append({
+                            "points": [(borehole_position_1, upper_depth_draw_1),
+                                        (borehole_position_2, upper_depth_draw_2),
+                                        (borehole_position_2, lower_depth_draw_2),
+                                        (borehole_position_1, lower_depth_draw_1)],
+                            "color": color_mapping[str(type_1)],
+                            "label": type_1 if type_1 not in legend_labels else None
+                        })
+                        matched_layers_1.add(idx_1)
+                        matched_layers_2.add(idx_2)
+                        legend_labels.add(type_1)
+                        upper_depth_draw_1 = lower_depth_draw_1
+                        upper_depth_draw_2 = lower_depth_draw_2
+                        count_1[type_1] -= 1
+                        count_2[type_2] -= 1
+                        break
+
+                        # 如果找不到最接近的Ic值，增加厚度为0的土层
+                    else:
+                        layers.append({
+                            "points": [(borehole_position_1, upper_depth_draw_1),
+                                        (borehole_position_2, upper_depth_draw_2),
+                                        (borehole_position_2, upper_depth_draw_2),
+                                        (borehole_position_1, lower_depth_draw_1)],
+                            "color": color_mapping[str(type_1)],
+                            "label": type_1 if type_1 not in legend_labels else None
+                        })
+                        matched_layers_1.add(idx_1)
+                        legend_labels.add(type_1)
+                        upper_depth_draw_1 = lower_depth_draw_1
+                        upper_depth_draw_2 = lower_depth_draw_2
+                        break
+
+                # 如果某一方土层數量為0
+                elif count_1[type_1] != 0 and count_1[type_2] == 0:
+                    layers.append({
+                        "points": [(borehole_position_1, upper_depth_draw_1),
+                                    (borehole_position_2, upper_depth_draw_2),
+                                    (borehole_position_2, upper_depth_draw_2),
+                                    (borehole_position_1, lower_depth_draw_1)],
+                        "color": color_mapping[str(type_1)],
+                        "label": type_1 if type_1 not in legend_labels else None
+                    })
+                    matched_layers_1.add(idx_1)
+                    legend_labels.add(type_1)
+                    upper_depth_draw_1 = lower_depth_draw_1
+                    upper_depth_draw_2 = lower_depth_draw_2
+                    break
+                elif count_1[type_1] == 0 and count_1[type_2] != 0:
+                    break
+
+# 初始化图形
 fig, ax = plt.subplots(figsize=(10, 6))
+
+# 用于存储图例条目的列表
+legend_handles = []
 
 # 绘制每个土壤层为多边形
 for layer in layers:
     points = layer["points"]
-    polygon = Polygon(points, closed=True, color=layer["color"], alpha=0.7, label=layer["name"])
+    polygon = Polygon(points, closed=True, color=layer["color"], alpha=0.7)
     ax.add_patch(polygon)
+
+    # 仅当需要添加图例时才绘制空的线以生成图例条目
+    if layer["label"] is not None:
+        handle, = ax.plot([], [], color=layer["color"], label=layer["label"])
+        legend_handles.append(handle)
 
 # 绘制钻孔位置
 ax.axvline(x=borehole_position_1, color='black', linestyle='--', linewidth=1, label='Borehole 1')
 ax.axvline(x=borehole_position_2, color='black', linestyle='--', linewidth=1, label='Borehole 2')
+
+# 设置图例框在图的右上角，绘图区的外侧
+ax.legend(handles=legend_handles, loc='upper right', bbox_to_anchor=(-0.05, 1))
 
 # 反转Y轴
 plt.gca().invert_yaxis()
 
 # 设置图形属性
 ax.set_xlim(0, 1600)
-ax.set_ylim(60, 0)  # 反转 y 轴范围，深度从上到下
-ax.set_title("Kriging of Soil Layers")
+ax.set_ylim(70, 0)  # 反转 y 轴范围，深度从上到下
+ax.set_title("Soil Type Visualization between Boreholes by Depth Sections")
 ax.set_xlabel("Distance")
 ax.set_ylabel("Depth")
-ax.legend(loc='upper right')
 
 plt.show()
-
--- line.py
+++ line.py
@@ -17,75 +17,87 @@ def read_data(file_path):
     data = pd.read_excel(file_path)
     return data
 
-# 統計每種土壤的深度範圍，並計算平均IC和平均qc（前200筆資料不納入計算）
+# 選擇儲存位置及名稱
+def select_save_file():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.asksaveasfilename(defaultextension='.xlsx')
+    if not file_path:  # 如果使用者取消了儲存對話框
+        print("未選擇儲存檔案。")
+        return None
+    print(f"Selected file: {file_path}")
+    return file_path
+
+# 統計每種土壤的深度範圍，並計算平均IC（前200筆資料不納入計算）
 def calculate_depth_statistics_with_qc_avg(df):
     depth_col = df['Depth (m)']
     type_col = df['合併後']
     ic_col = df['Ic']
-    qc_col = df['qc (MPa)']
-    fs_col = df['fs (MPa)']  # 新增fs列
-    mark = df['Mark2']
+    Mark_1 = df['Mark1']
+    Mark_2 = df['Mark2']
+    SBTn = df['SBTn']
+    Bq = df['Bq']
 
-    # 準備變量來記錄每段土壤的範圍、平均IC值和平均qc值
+    # 準備變量來記錄每段土壤的範圍和平均IC值
     result = []
-    current_type = type_col.iloc[200]  # 從第201筆資料開始
-    start_depth = depth_col.iloc[200]
+    current_type = type_col.iloc[0]  # 從第201筆資料開始
+    start_depth = depth_col.iloc[0]
 
     ic_values = []
-    qc_values = []
 
     # 遍歷每一行，從第201筆開始，當遇到土壤類型變化或標記改變時，記錄當前土壤段的範圍
+# 遍歷每一行，從第201筆開始，當遇到土壤類型變化或標記改變時，記錄當前土壤段的範圍
     for i in range(201, len(df)):
-        # 確認是否符合計算條件
-        if mark.iloc[i] != '*' and pd.notna(fs_col.iloc[i]) and ic_col.iloc[i] != 0:
-            # 如果類型沒變，繼續添加該段的IC和qc值
-            ic_values.append(ic_col.iloc[i])
-            qc_values.append(qc_col.iloc[i])
-
-        # 當類型變化或標記變化時，記錄當前段的數據
-        if type_col.iloc[i] != current_type and mark.iloc[i] != '*':
+        # 總是記錄範圍，不受條件影響
+        if type_col.iloc[i] != current_type:  # 當類型變化時，記錄當前段的數據
             # 記錄當前土壤的範圍
             end_depth = depth_col.iloc[i - 1]
             if len(ic_values) > 0:  # 確保列表不為空
                 average_ic = sum(ic_values) / len(ic_values)  # 計算該段土壤的平均IC
             else:
                 average_ic = None  # 如果沒有符合條件的IC，記為 None
-            
-            average_qc = sum(qc_values) / len(qc_values) if len(qc_values) > 0 else None  # 計算該段土壤的平均qc
 
             # 添加到結果中
-            result.append([current_type, start_depth, end_depth, average_ic, average_qc])
+            result.append([current_type, start_depth, end_depth, average_ic])
 
             # 更新當前土壤類型和新的開始深度
             current_type = type_col.iloc[i]
             start_depth = depth_col.iloc[i]
             ic_values = []  # 重置IC值列表
-            qc_values = []  # 重置qc值列表
 
-    # 記錄最後一段土壤的範圍及平均IC和平均qc值
+        # 僅在條件符合時記錄 Ic 值
+        if Mark_2.iloc[i] != '*' and pd.notna(Bq.iloc[i]) and Bq.iloc[i] != 0 and Mark_1.iloc[i] != '*':
+            ic_values.append(ic_col.iloc[i])
+
+    # 記錄最後一段土壤的範圍及平均IC值
     end_depth = depth_col.iloc[-1]
     if len(ic_values) > 0:
         average_ic = sum(ic_values) / len(ic_values)
     else:
         average_ic = None
-    
-    average_qc = sum(qc_values) / len(qc_values) if len(qc_values) > 0 else None
-    result.append([current_type, start_depth, end_depth, average_ic, average_qc])
+    result.append([current_type, start_depth, end_depth, average_ic])
+
+
 
     # 創建 DataFrame 保存結果
-    depth_stats_df = pd.DataFrame(result, columns=['Type', 'Upper Depth', 'Lower Depth', 'Average IC', 'Average qc'])
-    
-    print("土壤類型深度統計和平均 IC 以及平均 qc（排除前200筆資料，且排除 Mark2 有 *、fs 缺值或 Ic=0 的數據）：")
+    depth_stats_df = pd.DataFrame(result, columns=['Type', 'Upper Depth', 'Lower Depth', 'Average Ic'])
+
+    print("土壤類型深度統計和平均 IC（排除前200筆資料，且排除 Mark2 有 *、SBTn=0 或 Mark1 有 * 的數據）：")
     print(depth_stats_df)
 
-    # 將結果保存為 Excel 文件
-    depth_stats_df.to_excel('soil_depth_statistics_with_ic_and_qc_avg_skipping_invalid.xlsx', index=False, engine='openpyxl')
+    # 選擇保存路徑
+    save_path = select_save_file()
+    if save_path:  # 如果成功選擇保存路徑，則保存文件
+        depth_stats_df.to_excel(save_path, index=False)
+        print(f"結果已保存到: {save_path}")
+    else:
+        print("未保存檔案。")
     
     return depth_stats_df
 
 # 繪製數據圖像
 def plot_data(df):
-    # 假設 df 包含 'Depth', 'qc (MPa)', '合併後' 三列
+    # 假設 df 包含 'Depth', 'Ic', '合併後' 三列
     plot_depth = df['Depth (m)']
     plot_value = df['Ic']
     plot_type = df['合併後']
@@ -106,7 +118,7 @@ def plot_data(df):
     # 创建图形
     fig, ax = plt.subplots(figsize=(6, 12))
     
-    # x軸範圍 0-70，y軸範圍 0-110
+    # x軸範圍 0-5，y軸範圍 0-110
     plt.xlim(0, 5)
     plt.ylim(0, 110)
 
@@ -129,19 +141,19 @@ def plot_data(df):
     # 添加標籤和標題
     plt.xlabel('Ic')
     plt.ylabel('Depth (m)')
-    plt.title('50cm-02 qc and soil type')
+    plt.title('50cm-02 Ic and soil type')
     
     # 添加網格
     plt.grid(linestyle='--', linewidth=0.5)
     
     # 設置 x 和 y 軸的刻度
-    x_major_locator = plt.MultipleLocator(5)
-    y_major_locator = plt.MultipleLocator(2)
+    x_major_locator = plt.MultipleLocator(1)
+    y_major_locator = plt.MultipleLocator(10)
     ax.xaxis.set_major_locator(x_major_locator)
     ax.yaxis.set_major_locator(y_major_locator)
 
     # 保存圖片
-    plt.savefig('50m-02_qc_and_soil_type.png')
+    plt.savefig('50m-02_Ic_and_soil_type.png')
 
     # 顯示圖片
     plt.show()
@@ -157,5 +169,5 @@ if __name__ == "__main__":
     # 繪製圖像
     plot_data(new_df)
 
-    # 計算深度範圍及平均 IC 和平均 qc，並保存結果於一個新的 xlsx 文件
+    # 計算深度範圍及平均 IC，並保存結果於一個新的 xlsx 文件
     calculate_depth_statistics_with_qc_avg(new_df)
